949055e2243bc223e8385555bcb71ce27cf0aa94
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-04-17 16:50:47 +0000

    changed formatting and fixed code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,83 +7,84 @@ This module is a Micro:bit MicroPython program that moves sprite around microbit
 from microbit import *
 
 # variables needed
-sprite = None
 loopCounterX = 0
 loopCounterY = 0
 
 # setting up
 display.clear()
 display.show(Image.HAPPY)
-
-# pressing A, sprite moves clockwise
 while True:
+
+    # when A's pressed, the pixels move clockwise
     if button_a.was_pressed():
+
+        # setup
         display.clear()
-        sprite = game.create_sprite(0, 0)
         loopCounterX = 0
         loopCounterY = 0
 
-        # moving sprite right
-        while loopCounterY <= 4:
-            sprite.set_x(loopCounterY)
-            loopCounterY += 1
-            sleep(300)
-
-        # moving sprite down
-        while loopCounterX <= 4:
-            sprite.set_y(loopCounterX)
+        # goes left to right
+        while loopCounterX < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
             loopCounterX += 1
-            sleep(300)
 
-        # moving sprite left
-        while loopCounterY >= 0:
-            sprite.set_x(loopCounterY)
-            loopCounterY -= 1
-            sleep(300)
+        # goes up to down
+        while loopCounterY < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY += 1
 
-        # moving sprite up
-        while loopCounterX >= 0:
-            sprite.set_y(loopCounterX)
+        # goes right to left
+        while loopCounterX > 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
             loopCounterX -= 1
-            sleep(300)
 
-        # resetting
-        sprite.delete()
-        display.clear()
+        # goes down to up
+        while loopCounterY >= 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY -= 1
         display.show(Image.HAPPY)
 
-    # pressing B, sprite moves counter clockwise
+    # when B's pressed, the pixels move counter clock wise
     if button_b.was_pressed():
+
+        # setup
         display.clear()
-        sprite = game.create_sprite(0, 0)
         loopCounterX = 0
         loopCounterY = 0
 
-        # moving sprite down
-        while loopCounterX <= 4:
-            sprite.set_y(loopCounterX)
-            loopCounterX += 1
-            sleep(300)
+        #  goes up to down
+        while loopCounterY < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY = loopCounterY + 1
 
-        # moving sprite right
-        while loopCounterY <= 4:
-            sprite.set_x(loopCounterY)
-            loopCounterY += 1
-            sleep(300)
+        #  goes left to right
+        while loopCounterX < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterX = loopCounterX + 1
 
-        # moving sprite up
+        #  goes down to up
+        while loopCounterY > 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY = loopCounterY - 1
+
+        # goes right to left
         while loopCounterX >= 0:
-            sprite.set_y(loopCounterX)
-            loopCounterX -= 1
-            sleep(300)
-
-        # moving sprite left
-        while loopCounterY >= 0:
-            sprite.set_x(loopCounterY)
-            loopCounterY -= 1
-            sleep(300)
-
-        # resetting
-        sprite.delete()
-        display.clear()
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterX = loopCounterX - 1
         display.show(Image.HAPPY)
